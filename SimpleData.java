0755f4c595c9c810e6126fc3a4dda64d9ae7a001
JuanAndresVallejo <jvallejo.devops@gmail.com>
2025-09-09 20:56:00 -0500

    feat: Upload of Simple Data task

---
+++ SimpleData.java
@@ -0,0 +1,17 @@
+public class SimpleData {
+
+    public static void main(String[] args) {
+        
+        int age = 26;
+        double height = 1.71;
+        boolean student = true;
+        char initial = 'J';
+        String name = "Juan Andres";
+
+        System.out.println("Hola!, mi nombre es " + name + ", tengo " + age + " años, mido " + height + " metros y mi inicial es la " + initial);
+        System.out.println("Soy estudiante? " + student);
+
+
+    }
+    
+}
