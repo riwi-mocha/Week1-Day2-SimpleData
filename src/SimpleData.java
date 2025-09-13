e8da8421920f0545f607c25aa9b49a66ef405beb
Andres Restrepo <andresrpoochoa@gmail.com>
2025-09-13 16:14:41 -0500

    Update personal data and fix class naming convention

---
+++ src/SimpleData.java
@@ -0,0 +1,22 @@
+public class SimpleData{
+
+    public static void main(String[] args) {
+        
+        String name = "Andres";
+        int age = 27;
+        double height = 1.80;
+        boolean student = true;
+        char initial = name.charAt(0);
+
+        System.out.println("Hello, my name is " + name + ", I'm " + age + " years old, and I'm " + height + " meters tall and my initial is: " + initial + ".");
+
+
+        if(student){
+            System.out.println("Soy un estudiante.");
+        } else {
+            System.out.println("No soy un estudiante.");
+        }
+
+    }
+
+}
